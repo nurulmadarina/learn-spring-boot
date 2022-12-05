defd9bd5de20e75c97b095c9933ff96179afcf07
Nurul Madarina <v-nurul.madarina@anteraja.id>
2022-12-05 20:34:27 +0700

    first commit

---
+++ src/main/java/com/mada/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,21 @@
+package com.mada.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Course: id, name, author
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn Backend", "byIn28Minutes")
+				);
+	}
+
+}
